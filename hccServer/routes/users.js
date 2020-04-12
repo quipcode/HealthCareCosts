f3be83aa282d690a4ceef8e413d6942b7ac63cda
quipcode <abdulkarim8ahmed@gmail.com>
2020-04-11 17:20:04 -0700

    getting stuck w/ bcrypt

--- hccServer/routes/users.js
+++ hccServer/routes/users.js
@@ -7,11 +7,41 @@ const passport = require("passport");
 
 
 
-const validateLoginInput = require('../validation/login')
 
+const validateLoginInput = require('../validation/login')
+const validateRegisterInput = require('../validation/register')
 //Load User model
 const User = require('../models/user')
 
+router.post('/register', (req,res) => {
+    const {errors, isValid} = validateRegisterInput(req.body)
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
+    User.findOne({email: req.body.email}).then(user => {
+        if(user){
+            return res.status(400).json({email: "Email already exists"})
+        }else{
+            const newUser = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+            })
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                  if (err) throw err;
+                  newUser.password = hash;
+                  newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                });
+              });
+        }
+    })
+})
 
 router.post("/login", (req,res) => {
     const {errors, isValid} = validateLoginInput(req.body)
@@ -20,35 +50,48 @@ router.post("/login", (req,res) => {
     }
     const email = req.body.email
     const password = req.body.password
-
+  
     User.findOne({email}).then(user => {
         if(!user) return res.status(404).json({emailnotfound: "Email not found"})
-        
-        bcrypt.compare(password, user.password).then(isMatch => {
-            if(isMatch){
-                const payload = {
-                    id: user.id,
-                    username: user.username
-                }
-                jwt.sign(
-                    payload,
-                    keys.secretOrKey,
-                    {
-                        expiresIn: 31556926 //1 year in sec
-                    },
-                    (err, token) => {
-                        res.json({
-                            success: true,
-                            token: "Bearer " + token
-                        })
-                    }
-                )
-            }else {
-                return res
-                    .status(400)
-                    .json({passwordincorrect: "Password incorrect"})
-            }
-        })
+        if(user){
+            res.json({
+                success: true,
+                userexists: true,
+                user: user
+            })
+        }
+        // bcrypt.compare(password, user.password).then(isMatch => {
+        //     if(isMatch){
+        //         const payload = {
+        //             id: user.id,
+        //             username: user.username
+        //         }
+        //         jwt.sign(
+        //             payload,
+        //             keys.secretOrKey,
+        //             {
+        //                 expiresIn: 31556926 //1 year in sec
+        //             },
+        //             (err, token) => {
+        //                 res.json({
+        //                     success: true,
+        //                     token: "Bearer " + token
+        //                 })
+        //             }
+        //         )
+        //     }else {
+        //         return res
+        //             .status(400)
+        //             .json({passwordincorrect: "Password incorrect"})
+        //     }
+        // })
+    })
+})
+
+router.get("/",(req,res) =>{
+    res.json({
+        success: true,
+        message: "sup"
     })
 })
 
