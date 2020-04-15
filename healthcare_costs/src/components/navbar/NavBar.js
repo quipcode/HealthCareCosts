758856377cefffc7e8fa85217931ec2ba776aec9
quipcode <abdulkarim8ahmed@gmail.com>
2020-04-14 23:53:20 -0700

    added functional login icon to navbar

--- healthcare_costs/src/components/navbar/NavBar.js
+++ healthcare_costs/src/components/navbar/NavBar.js
@@ -1,7 +1,7 @@
 import React, {useState}from 'react'
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, Col} from 'reactstrap'
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, Button, Col} from 'reactstrap'
 import { NavDropdown } from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // import React from 'react'
@@ -35,14 +35,18 @@ let NavBar = (props) => {
               Welcome back {auth.username}
               </NavItem>
               <NavItem>
-              <NavLink tag={Link} to="/logout">Logout</NavLink>
+                  <NavLink className="nav-link" to="/logut">
+                      <i className="fa fa-sign-out fa-lg" /> Logout 
+                  </NavLink>
+                <NavLink tag={Link} to="/logout">Logout</NavLink>
               </NavItem>
         </Nav>
       // </Collapse>
         : 
-    <Navbar dark sticky="top"  className=" navbar navbar-dark  navbar-expand-sm" expand="sm"  >
-          <NavbarBrand className="mr-auto" href="/"><img src="../assets/images/2018-healthcare-costs.jpg" height="30" width="30" alt="Cost of HealthCare" /></NavbarBrand>
-          <NavbarToggler onClick={() =>toggler(!toggleNav)} />
+    // <Navbar dark sticky="top"  className=" navbar navbar-dark  navbar-expand-sm" expand="sm"  >
+    
+          // <React.Fragment>
+          
           <Collapse isOpen={toggleNav} navbar>
             <Nav className="float-xs-right" navbar>
               <NavDropdown eventkey={1}
@@ -66,14 +70,35 @@ let NavBar = (props) => {
                 <NavLink tag={Link} to="/login">Log in</NavLink>
               </NavItem>
             </Nav>
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                {/* <Button outline onClick={this.toggleModal}> */}
+                  <Button  color="secondary">
+                      <i className="fa fa-sign-in fa-lg" /> Login
+                      {/* {this.props.auth.isFetching ?
+                          <span className="fa fa-spinner fa-pulse fa-fw"></span>
+                          : null
+                      } */}
+                  </Button>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/login">
+                      <i className="fa fa-sign-in fa-lg" /> Login 
+                  </NavLink>
+                  {/* <NavLink tag={Link} to="/login">Log in</NavLink> */}
+              </NavItem>
+            </Nav>
           </Collapse>
-    </Navbar>
+          // </React.Fragment>
+    //</Navbar>
     }
     return(
         <div>
-            <Navbar>
+            <Navbar dark sticky="top"  className=" navbar navbar-dark  navbar-expand-sm" expand="sm">
             {/* <Navbar color="inverse" dark full> */}
-                <NavbarBrand href="/">Our Cool App</NavbarBrand>
+                {/* <NavbarBrand href="/">Our Cool App</NavbarBrand> */}
+                <NavbarBrand className="mr-auto" href="/"><img src="../assets/images/2018-healthcare-costs.jpg" height="30" width="30" alt="Cost of HealthCare" /></NavbarBrand>
+                <NavbarToggler onClick={() =>toggler(!toggleNav)} />
                 {loginOrProfile(props.auth)}
             </Navbar>
         </div>
